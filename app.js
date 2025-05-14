895c840519a6e82c6457f4e9e44b16c293ab83ce
Joanne <joannekjy@hotmail.com>
2025-05-14 22:33:47 +0800

    render avatar on board

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const game = {
   players: 2,
   avatar: ["Burgerman", "Friesman"],
   playerTurn: "",
-  difficultyLevel: [0, 1, 2], // extract level here
+  difficultyLevel: "", // extract level here
   dice: [1, 2, 3, 4, 5, 6],
   badFood: [14],
   toilet: [], // do separate array for the badfood and toilet
@@ -21,8 +21,8 @@ const game = {
 };
 
 const players = [
-  { player: "Player 1", avatar: "", currPos: 0 },
-  { player: "Player 2", avatar: "", currPos: 0 },
+  { player: "Player 1", avatar: "", prevPos: 0, currPos: 0 },
+  { player: "Player 2", avatar: "", prevPos: 0, currPos: 0 },
 ];
 
 /*------------------------ Cached Element References ------------------------*/
@@ -51,6 +51,19 @@ const nameSubmit = document.getElementById("playersubmit");
 //     // return;
 //   });
 // };
+const level = () => {
+  const easy = document.getElementById("easy");
+  const med = document.getElementById("med");
+  const hard = document.getElementById("hard");
+
+  easy.addEventListener("click", () => {
+    game.difficultyLevel = "easy";
+  });
+};
+level();
+
+console.log(game.difficultyLevel);
+
 const playerNumber = () => {
   for (let i = 2; i < game.players; i++) {
     if ((i = 3)) {
@@ -87,7 +100,7 @@ const rows = () => {
 };
 rows();
 
-const gameBoard = () => {
+const gameBoardEasy = () => {
   for (let i = 0; i < 30; i++) {
     if (i < 5) {
       const newSqr = document.createElement("div");
@@ -95,6 +108,7 @@ const gameBoard = () => {
       row1.appendChild(newSqr);
       const boxName = i + 1;
       newSqr.classList.add("square");
+      newSqr.setAttribute("id", boxName);
       newSqr.textContent = boxName;
     } else if (i > 4 && i < 10) {
       const newSqr = document.createElement("div");
@@ -102,6 +116,7 @@ const gameBoard = () => {
       row2.appendChild(newSqr);
       const boxName = i + 1;
       newSqr.classList.add("square");
+      newSqr.setAttribute("id", boxName);
       newSqr.textContent = boxName;
     } else if (i > 9 && i < 15) {
       const newSqr = document.createElement("div");
@@ -109,6 +124,7 @@ const gameBoard = () => {
       row3.appendChild(newSqr);
       const boxName = i + 1;
       newSqr.classList.add("square");
+      newSqr.setAttribute("id", boxName);
       newSqr.textContent = boxName;
     } else if (i > 14 && i < 20) {
       const newSqr = document.createElement("div");
@@ -116,6 +132,7 @@ const gameBoard = () => {
       row4.appendChild(newSqr);
       const boxName = i + 1;
       newSqr.classList.add("square");
+      newSqr.setAttribute("id", boxName);
       newSqr.textContent = boxName;
     } else if (i > 19 && i < 25) {
       const newSqr = document.createElement("div");
@@ -123,6 +140,7 @@ const gameBoard = () => {
       row5.appendChild(newSqr);
       const boxName = i + 1;
       newSqr.classList.add("square");
+      newSqr.setAttribute("id", boxName);
       newSqr.textContent = boxName;
     } else {
       const newSqr = document.createElement("div");
@@ -130,11 +148,12 @@ const gameBoard = () => {
       row6.appendChild(newSqr);
       const boxName = i + 1;
       newSqr.classList.add("square");
+      newSqr.setAttribute("id", boxName);
       newSqr.textContent = boxName;
     }
   }
 };
-gameBoard();
+gameBoardEasy();
 playerNameInput();
 
 const playerAvatar = () => {
@@ -154,13 +173,7 @@ const playerAvatar = () => {
     game.playerTurn = players[0].player;
     game.message =
       "Hey " + players[0].player + ", you choose Burger Man. You roll first.";
-    // console.log(players[0].player);
-    // console.log(players[0].avatar);
-    // console.log(game.playerTurn);
-    // console.log(game.message);
-    // console.log(game, players);
     renderMessage();
-
     return;
   });
 
@@ -177,32 +190,49 @@ const playerAvatar = () => {
     game.playerTurn = players[0].player;
     game.message =
       "Hey " + players[0].player + ", you choose Fries Man. You roll first.";
-    // console.log(players[0].player);
-    // console.log(players[0].avatar);
-    // console.log(game.playerTurn);
-    // console.log(game.message);
-    // console.log(game, players);
     renderMessage();
+    return;
   });
-  return;
 };
 playerAvatar();
 
 const posTurnUpdate = (result) => {
-  if (game.playerTurn === players[0].player) {
-    players[0].currPos += result;
-    game.playerTurn = players[1].player;
+  const player1 = players[0];
+  const player2 = players[1];
+
+  if (game.playerTurn === player1.player) {
+    player1.currPos += result;
+    game.playerTurn = player2.player;
     game.message = "It's " + game.playerTurn + " turn!";
+    // let currBox = document.getElementById(players[0].currPos)
+
+    let currentPos = player1.currPos;
+    if (player1.avatar === "Burgerman") {
+      burgerAva.style.display = "none";
+      renderBurger(currentPos);
+    } else if (player1.avatar === "Friesman") {
+      renderFries(currentPos);
+    }
     renderMessage();
 
     // console.log(players[0].currPos);
     // console.log(game.playerTurn);
     // console.log(game.message);
     // console.log(game, players);
-  } else if (game.playerTurn === players[1].player) {
-    players[1].currPos = players[1].currPos + result;
-    game.playerTurn = players[0].player;
+  } else if (game.playerTurn === player2.player) {
+    player2.currPos = player2.currPos + result;
+    game.playerTurn = player1.player;
     game.message = "It's " + game.playerTurn + " turn!";
+
+    let currentPos = player2.currPos;
+    if (player2.avatar === "Burgerman") {
+      burgerAva.style.display = "none";
+      renderBurger(currentPos);
+    } else if (player1.avatar === "Friesman") {
+      friesAva.style.display = "none";
+      renderFries(currentPos);
+    }
+
     renderMessage();
 
     // console.log(players[1].currPos);
@@ -211,6 +241,8 @@ const posTurnUpdate = (result) => {
     // console.log(game, players);
   }
 };
+
+//   //   }
 
 const dice = () => {
   diceroll.addEventListener("click", () => {
@@ -233,8 +265,58 @@ const renderRotten = () => {
   winPopup.setAttribute("src", "");
   winPopup.setAttribute("alt", "Rotten food");
 };
-
+if (players[0].avatar === "Burgerman") {
+}
+const renderBurger = (currentPos) => {
+  let currAvaOnBoard = document.createElement("img");
+  currAvaOnBoard.setAttribute("src", "pictures/burger.png");
+  currAvaOnBoard.setAttribute("alt", "Burgerman");
+  currAvaOnBoard.classList.add("avatar");
+  let currBox = document.getElementById(currentPos);
+  currBox.appendChild(currAvaOnBoard);
+};
+const renderFries = (currentPos) => {
+  let currAvaOnBoard = document.createElement("img");
+  currAvaOnBoard.setAttribute("src", "pictures/fries_sunglass.png");
+  currAvaOnBoard.setAttribute("alt", "Friesman");
+  currAvaOnBoard.classList.add("avatar");
+  let currBox = document.getElementById(currentPos);
+  currBox.appendChild(currAvaOnBoard);
+};
 // renderRotten();
+let boardBoxes = document.querySelectorAll(".square");
+
+// const currAva = () => {
+//   //   if (game.difficultyLevel === "easy") {
+//   // let currBox = document.getElementById(players[j].currPos);
+//   for (let i = 0; i < 30; i++) {
+//     if (Number(boardBoxes[i].textContent) === players[j].currPos) {
+//       // boardBoxes[i].textContent = "testing";
+//       // if (players[j].currPos !== 0) {
+//       //     players[j].prevPos = players[j].currPos
+//       // }
+//       if (players[0].avatar === "Burgerman") {
+//         burgerAva.style.display = "none";
+//         renderBurger(j);
+//         break;
+//       } else if (players[0].avatar === "Friesman") {
+//         friesAva.style.display = "none";
+//         renderFries(j);
+//         break;
+//       }
+//     }
+//   }
+
+//   //   }
+// };
+// console.log(game.difficultyLevel);
+// console.log(game.players);
+
+// console.log(players);
+console.log(boardBoxes);
+
+// console.log(players[0].currPos);
+// console.log(players[1].currPos);
 
 //   const currAvatar = () => {
 //     let boardBox = document.querySelector(".board");
